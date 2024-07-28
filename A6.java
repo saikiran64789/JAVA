57f87b8dbc5b71fb88fb2d2e49cfe56772f7e0b0
saikiran64789 <soorasaikiran607@gmail.com>
2024-07-28 11:05:42 +0530

    Add files via upload

---
+++ A6.java
@@ -0,0 +1,16 @@
+interface printable{
+    void print();
+}
+
+class A6 implements printable{
+    public void print()
+{
+    System.out.println("saikiranindra");
+}
+public static void main(String args[])
+{
+    A6 obj  = new A6();
+    obj.print();
+}
+
+}
